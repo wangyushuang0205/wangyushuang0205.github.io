f30becf767718894451a76853fa8e9c7ef57ccb4
17866627884 <1597962291@qq.com>
2019-11-15 18:02:43 +0800

    build

--- src/components/slicing/pc/slicingList.vue
+++ src/components/slicing/pc/slicingList.vue
@@ -4,13 +4,13 @@
     <div class="slicing_menu">
       <ul>
         <span class="menu_title">模板分类</span>
-        <li v-if="item.isShow" v-for="(item,index) in menus" :key="index" @click="push(item,1,index)" class="first">
+        <li v-if="item.isShow" v-for="(item,index) in menus" :key="index" @click="push(item,1,index)" class="handle">
           <span>{{item.label}}</span>
         </li>
       </ul>
     </div>
     <div class="background">
-      <div v-for="(item, index) in slicingList" :key="index" @click="$router.push({name:item.target})" class="card">
+      <div v-for="(item, index) in slicingList" :key="index" @click="$router.push({name:item.target})" class="card hidden">
         <div class="img_inner"><img :src="item.url" class="img_top"></div>
         <p class="card_title">{{item.label}}--{{item.title}}</p>
       </div>
@@ -38,13 +38,15 @@ export default {
       color  : '',
       slicingList: [
         { 
+          isShow: true,
           title : '这是我的第1个切图', 
           label : '个人博客',
-          type  : 'recommend',
+          type  : 'blog',
           url   : 'https://timg.qifeiye.com/manual/54d4a4629fb6b380x280_1.png', 
-          target: 'slicingOne',
+          target: 'email1',
         },
         { 
+          isShow: true,
           title : '这是我的第2个切图', 
           label : '推荐模板',
           type  : 'recommend',
@@ -52,35 +54,56 @@ export default {
           target: 'slicingOne', 
         },
         { 
+          isShow: true,
           title : '这是我的第3个切图', 
           label : '商业模板',
-          type  : 'recommend',
+          type  : 'business',
           url   : 'https://timg.qifeiye.com/manual/5d6cac958e71f380x280_1.png', 
           target: 'slicingOne', 
         },
         { 
+          isShow: true,
           title : '这是我的第4个切图', 
           label : '推荐模板',
           type  : 'recommend',
           url   : 'https://timg.qifeiye.com/manual/5d6791348892b380x280_1.png', 
           target: 'slicingOne', 
-      },
+        },
         { 
+          isShow: true,
           title : '这是我的第5个切图', 
           label : '推荐模板',
           type  : 'recommend',
           url   : 'https://timg.qifeiye.com/manual/5d8c6f506826a380x280_1.png', 
           target: 'slicingOne', 
-      },
+        },
       ],
     }
   },
   methods: {
     push(item,tier,index){
-      if (item.hasChild == false) {
-        this.$router.push({name:item.target});
+      // if (item.hasChild == false) {
+      //   this.$router.push({name:item.target});
+      // }
+      // console.log(item.type,tier,index);
+      $(".card").eq(0).addClass("blog");
+      $(".card").eq(1).addClass("recommend");
+      $(".card").eq(2).addClass("business");
+      $(".card").eq(3).addClass("recommend");
+      $(".card").eq(4).addClass("recommend");
+      if($(".card").is(".blog")){
+        $(".blog").removeClass("hidden");
+      }else{
+        $(".hidden1").hide();
       }
-      // console.log(item.index);
+      if($(".card").is(".recommend")){
+        $(".recommend").removeClass("hidden");
+        $(".hidden").hide();
+      }
+      if($(".card").is(".business")){
+        $(".business").removeClass("hidden");
+        $(".hidden").hide();
+      }
       // $(function(){
       //   var bgCounter  = 0;
       //   var bgList     = [];
@@ -102,21 +125,38 @@ export default {
       // });
     },    
     init() {
-      if (this.base.mp.header.isShow) {
-        this.menus = this.base.mp.header.children;
-      }
-      // this.list = this.company.img_view;
+      //这里是图片上下滚动
+      // let window_width  = $(window).width();
+      // if (window_width < 9999) {
+        var div = $('.img_top');
+        $.each(div, function(){
+          let img_height   = $(this).height();
+          let img_inner    = $(".img_inner").height();
+          let moveY        = img_height-img_inner;
+          let y1 = 'translateY' + '(' + (-moveY) + 'px' + ')';
+          let y2 = 'translateY' + '(' + 0 + 'px' + ')';
+          $(this).mouseover(function () {
+            // console.log(-moveY,"鼠标移入");
+            $(this).css({
+              'transform': y1, 
+              'transition':'all 3.5s linear 0s'
+            });
+          }).mouseout(function () {
+            // console.log(moveY,"鼠标移出");
+            $(this).css({
+              'transform': y2, 
+              'transition':'all 1.5s linear 0s'
+            });
+          });
+        });
+      // }
     },
   },
   mounted() {
+    this.init();
     $(document).ready(function(){
-        let window_height  = $(".background").height();
-        $(".slicing_menu").css('height', window_height);
-        var div = $('.img_top');
-        $.each(div, function(){
-
-          //这里写图片上下滚动
-        });
+      let window_height  = $(".background").height();
+      $(".slicing_menu").css('height', window_height);
       $(window).resize(function() {
         let window_height  = $(".background").height();
         $(".slicing_menu").css('height', window_height);
@@ -177,7 +217,7 @@ export default {
         padding: 20px auto;
         margin: 20px auto;
       }
-      .first {
+      .handle {
         width: 80%;
         margin: 10px auto;
         list-style: none;
